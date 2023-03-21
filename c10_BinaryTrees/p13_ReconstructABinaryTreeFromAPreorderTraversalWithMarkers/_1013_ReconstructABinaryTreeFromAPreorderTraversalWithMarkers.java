338a126a35e3d3298eee2814a2cd3e3da904a4c8
Andy <snoopa477@gmail.com>
2023-03-21 23:57:58 +0800

    10.13

--- c10_BinaryTrees/p13_ReconstructABinaryTreeFromAPreorderTraversalWithMarkers/_1013_ReconstructABinaryTreeFromAPreorderTraversalWithMarkers.java
+++ c10_BinaryTrees/p13_ReconstructABinaryTreeFromAPreorderTraversalWithMarkers/_1013_ReconstructABinaryTreeFromAPreorderTraversalWithMarkers.java
@@ -1,7 +1,28 @@
 package c10_BinaryTrees.p13_ReconstructABinaryTreeFromAPreorderTraversalWithMarkers;
+
 import static java.lang.System.out;
+
+import java.util.List;
+
 public class _1013_ReconstructABinaryTreeFromAPreorderTraversalWithMarkers {
+	private static Integer subtreeldx;
 
+	public static BinaryTreeNode<Integer> reconstructPreorder(List<Integer> preorder) {
+		subtreeldx = 0;
+		return reconstructPreorderSubtree(preorder);
+	}
+
+	// Reconstructs the subtree that is rooted at subtreeldx .
+	private static BinaryTreeNode<Integer> reconstructPreorderSubtree(List<Integer> preorder) {
+		Integer subtreeKey = preorder.get(subtreeldx);
+		++subtreeldx;
+		if (subtreeKey == null) {
+			return null;
+		}
+		// Note that reconstructPreorderSubtree updates subtreeldx. So the order
+		// following two calls are critical.
+		BinaryTreeNode<Integer> leftSubtree = reconstructPreorderSubtree(preorder);
+		BinaryTreeNode<Integer> rightSubtree = reconstructPreorderSubtree(preorder);
+		return new BinaryTreeNode(subtreeKey, leftSubtree, rightSubtree);
+	}
 }
-
-
