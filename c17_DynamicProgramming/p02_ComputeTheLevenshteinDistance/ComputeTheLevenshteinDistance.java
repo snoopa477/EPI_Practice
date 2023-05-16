9655825ed8ca594c70ee5f05f0199508467f7c36
Andy <snoopa477@gmail.com>
2023-05-17 00:36:22 +0800

    17.01 WIP

---
+++ c17_DynamicProgramming/p02_ComputeTheLevenshteinDistance/ComputeTheLevenshteinDistance.java
@@ -0,0 +1,157 @@
+package c17_DynamicProgramming.p02_ComputeTheLevenshteinDistance;
+import static java.lang.System.out;
+
+import java.util.Arrays;
+public class ComputeTheLevenshteinDistance {
+
+	private static int NONVISITED = -1;
+	
+	public static int levenshteinDistance(String A, String B) {
+		
+		/* notation:
+		 * str[ startIndx, endIndx Inclusive ]
+		 * 
+		 * given
+		 * L( A[0, a-1], B[0, b-1]) = x
+		 * then we denote 
+		 * levenshteinDistance[ A.length() -1 ][ B.length() -1 ] = x
+		 * 
+		 * L( A[0, i], B[0, j]) = x
+		 * levenshteinDistance[ i ][ j ] = x
+		 * 
+		 * row i is A's index, which is A.substring(0, row + 1)
+		 * row j is B's index, which is B.substring(0, col + 1)
+		 * value is levenshteinDistance
+		 */
+		
+		int LDistance[][] = new int[A.length()][B.length()];
+		
+		/* DETAIL:
+		 * since levenshteinDistance >= 0
+		 * we initialize it as -1 to denote the values has not yet computed( visited)
+		 */
+		for( int row[] : LDistance ) {
+			Arrays.fill(row, NONVISITED);
+		}
+	
+		
+		return getLevenshteinDistance( LDistance, A, A.length() -1, B, B.length() -1  );
+	}
+
+	
+	
+	private static int getLevenshteinDistance(int[][] LDistance, String A, int A_lastInclusiveIdx, String B, int B_lastInclusiveIdx ) {
+		
+		
+		/**
+		 * BASE CASE : determine the value without calling other function
+		 */
+		
+		/*which is -1, a empty string
+		 * 
+		 *            --A 
+		 * b b b b y  --B
+		 * 
+		 * A requires B_lastInclusiveIdx + 1 inserts
+		 */
+		if( A_lastInclusiveIdx < 0 ) {
+			//convert index to count
+			return B_lastInclusiveIdx + 1;
+		}
+		/*which is -1, a empty string
+		 * 
+		 * a a a a x  --A 
+		 *            --B
+		 * 
+		 * A requires A_lastInclusiveIdx + 1 deletes
+		 */
+		else if( B_lastInclusiveIdx < 0 ) {
+			return A_lastInclusiveIdx + 1;
+		}
+		
+		//if non visited, then compute, storing to cache; otherwise get the cache
+		if( LDistance[ A_lastInclusiveIdx ][ B_lastInclusiveIdx ] != NONVISITED ) {
+			
+			if( isLastCharTheSame( A, A_lastInclusiveIdx, B, B_lastInclusiveIdx ) ) {
+				
+				/**
+				 * example: 
+				 * a a a a x  --A 
+				 * b b b b x  --B
+				 * 
+				 * -----------------
+				 * a a a a)x  --A[0, A_lastInclusiveIdx -1] + x
+				 * b b b b)x  --B[0, B_lastInclusiveIdx -1] + x
+				 * 
+				 * last character x to x requires 0 operations
+				 */
+				LDistance[ A_lastInclusiveIdx ][ B_lastInclusiveIdx ] = LDistance[ A_lastInclusiveIdx -1 ][ B_lastInclusiveIdx -1 ] + 0 /*x to x: NONE*/;
+				
+			}
+			//answer lies within three of them\
+			/**
+			 * THINK_FURTHER:
+			 * three states all can reach to current state by one operation: 
+			 * A[0, A_lastInclusiveIdx], B[0, B_lastInclusiveIdx]; however, we only pick the minimum one among them
+			 */
+			else {
+				
+				/**
+				 * example: 
+				 * a a a a x  --A 
+				 * b b b b y  --B
+				 * 
+				 * -----------------
+				 * a a a a x)NULL  --A + NULL
+				 * b b b b)y       --B[0, B_lastInclusiveIdx -1 ] + y
+				 * 
+				 * last character NULL to y require one insertion
+				 */
+				int insertToLast = LDistance[ A_lastInclusiveIdx ][ B_lastInclusiveIdx -1 ] + 1 /*NULL to y: insert*/;
+				
+				/**
+				 * example: 
+				 * a a a a x  --A 
+				 * b b b b y  --B
+				 * 
+				 * -----------------
+				 * a a a a)x          --A[0, A_lastInclusiveIdx -1 ] + x
+				 * b b b b y)NULL     --B + NULL
+				 * 
+				 * last character x to NULL require one deletion
+				 */
+				int deleteToLast = LDistance[ A_lastInclusiveIdx -1 ][ B_lastInclusiveIdx ] + 1 /*x to NULL: deletion*/;
+				
+				/**
+				 * example: 
+				 * a a a a x  --A 
+				 * b b b b y  --B
+				 * 
+				 * -----------------
+				 * a a a a)x          --A[0, A_lastInclusiveIdx -1 ] + x
+				 * b b b b)y          --B[0, B_lastInclusiveIdx -1 ] + y
+				 * 
+				 * last character x to y needs one replace
+				 */
+				int replaceLast = LDistance[ A_lastInclusiveIdx -1 ][ B_lastInclusiveIdx -1 ] + 1 /*x to y: replace*/;
+				
+				int minValue = Math.min(insertToLast, Math.min( deleteToLast, replaceLast ));
+				LDistance[ A_lastInclusiveIdx ][ B_lastInclusiveIdx ] = minValue;
+			}
+			
+		}
+		
+		//reaching here meaning the value is either computed or just get from previous result
+		
+		return LDistance[ A_lastInclusiveIdx ][ B_lastInclusiveIdx ];
+	}
+	
+	
+	
+	private static boolean isLastCharTheSame( String A, int A_lastInclusiveIdx, String B, int B_lastInclusiveIdx ) {
+		return A.charAt(A_lastInclusiveIdx) == B.charAt(B_lastInclusiveIdx);
+	}
+	
+}
+
+
--- c17_DynamicProgramming/p02_ComputeTheLevenshteinDistance/_1702_ComputeTheLevenshteinDistance.java
+++ c17_DynamicProgramming/p02_ComputeTheLevenshteinDistance/_1702_ComputeTheLevenshteinDistance.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 public class _1702_ComputeTheLevenshteinDistance {
 
 	public static int levenshteinDistance(String A, String B) {
+		
 		int[][] distanceBetweenPrefixes = new int[A.length()][B.length()];
 		for (int[] row : distanceBetweenPrefixes) {
 			Arrays.fill(row, -1);
@@ -17,27 +18,29 @@ public class _1702_ComputeTheLevenshteinDistance {
 
 	}
 
-	private static int computeDistanceBetweenPrefixes(String A, int A_idx, String B, int B_idx,
-			int[][] distanceBetweenPrefixes) {
+	
+	
+	private static int computeDistanceBetweenPrefixes(String A, int A_idx, String B, int B_idx, int[][] distanceBetweenPrefixes) {
+		
 		if (A_idx < 0) {
-// A is empty so add all of B's characters.
+			// A is empty so add all of B's characters.
 			return B_idx + 1;
 		} else if (B_idx < 0) {
-// B is empty so delete all of A's characters.
+			// B is empty so delete all of A's characters.
 			return A_idx + 1;
-
 		}
+		
 
 		if (distanceBetweenPrefixes[A_idx][B_idx] == -1) {
+			
 			if (A.charAt(A_idx) == B.charAt(B_idx)) {
-				distanceBetweenPrefixes[A_idx][B_idx] = computeDistanceBetweenPrefixes(
-
-						A, A_idx - 1, B, B_idx - 1, distanceBetweenPrefixes);
-			} else {
-				int substituteLast = computeDistanceBetweenPrefixes(A, A_idx - 1, B, B_idx - 1,
-						distanceBetweenPrefixes);
+				distanceBetweenPrefixes[A_idx][B_idx] = computeDistanceBetweenPrefixes( A, A_idx - 1, B, B_idx - 1, distanceBetweenPrefixes );
+			} 
+			else {
+				int substituteLast = computeDistanceBetweenPrefixes(A, A_idx - 1, B, B_idx - 1, distanceBetweenPrefixes);
 				int addLast = computeDistanceBetweenPrefixes(A, A_idx, B, B_idx - 1, distanceBetweenPrefixes);
 				int deleteLast = computeDistanceBetweenPrefixes(A, A_idx - 1, B, B_idx, distanceBetweenPrefixes);
+				
 				distanceBetweenPrefixes[A_idx][B_idx] = 1 + Math.min(substituteLast, Math.min(addLast, deleteLast));
 			}
 
