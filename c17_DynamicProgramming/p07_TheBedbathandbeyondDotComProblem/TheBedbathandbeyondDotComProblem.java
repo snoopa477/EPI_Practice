0829783c5fe59bdc3b893d550db006970601b20e
Andy <snoopa477@gmail.com>
2023-06-01 00:48:48 +0800

    17.07 WIP

---
+++ c17_DynamicProgramming/p07_TheBedbathandbeyondDotComProblem/TheBedbathandbeyondDotComProblem.java
@@ -0,0 +1,86 @@
+package c17_DynamicProgramming.p07_TheBedbathandbeyondDotComProblem;
+import static java.lang.System.out;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+public class TheBedbathandbeyondDotComProblem {
+	
+	public static int EXCLUSIVE_OFFSET = 1;
+	public static int NEXT_OFFSET = 1;
+	public static int TO_SIZE_OFFSET = 1;
+	public static int NONVISITED = -1;
+	
+	public static List<String> decomposelntoDictionaryWords(String domain, Set<String> dictionary) {
+		
+		/**
+		 * PURPOSE: 
+		 * To solve domain at index domain.length-1
+		 * we need to solve domain at index 0 first
+		 * and then 1, 2, 3, .... domain.length -2
+		 * 
+		 * since we gonna use these sub results to construct our goal: index at domain.length-1
+		 */
+		
+		
+		/**
+		 * substrLastWordLength[i] = value
+		 * last word length 'value' at substring( 'domain', 0, 'i' + EXCLUSIVE_OFFSET ); 
+		 * in other words, the word ends at index 'i'
+		 *
+		 * REASONING: 
+		 * if substrLastWordLength[i] > 0 and value is not i + 1 ( the whole substring )
+		 * then there exist j, ( j < i ) that can be  
+		 *
+		  *THINK_FURTHER: wat we don't record is the word, but length, since we can infer the word 
+		  *by referencing 'domain' using substring and 'length' 
+		  */
+		int[] substrLastWordLength = new int[ domain.length() ];
+		Arrays.fill( substrLastWordLength , NONVISITED);
+		
+		
+		for( int i = 0; i < domain.length(); i++ ) {
+			
+			/**
+			 * PURPOSE: 
+			 * 	step 1: try whole string, without help from previous result
+			 * 	step 2: try sub string, with help from previous result 
+			 */
+			
+			//step 1: try whole string, without help from previous result
+			if(  dictionary.contains( domain.substring(0, i + EXCLUSIVE_OFFSET) ) ) {
+				//found, record the length; in other words, value > 0 means we found the matching
+				substrLastWordLength[ i ] = i + TO_SIZE_OFFSET;
+			}
+			//step 2: try sub string, with help from previous result 
+			else {
+				
+				//DETAIL: name suggests its behavior, not j, which doesn't match its behavior
+				//REASONING: try match the longest word, so we start with the shortest previous result: 0
+				for( int subI = 0; subI < i; subI++ ) {
+					
+						//previous result exists
+					if( substrLastWordLength[ subI ] != NONVISITED 
+						//current word that is right after the previous result
+						&& dictionary.contains( domain.substring(subI + NEXT_OFFSET, i + EXCLUSIVE_OFFSET) ) ) {
+						
+						// i - ( subI + NEXT_OFFSET ) + TO_SIZE_OFFSET = i - subI
+						substrLastWordLength[ i ] = i + i - subI;
+						//stop further trying. we find the longest match
+						break;
+					}
+				}
+				
+			}
+		}
+		
+		
+		
+		return null;
+	}
+
+}
+
+
--- c17_DynamicProgramming/p07_TheBedbathandbeyondDotComProblem/_1707_TheBedbathandbeyondDotComProblem.java
+++ c17_DynamicProgramming/p07_TheBedbathandbeyondDotComProblem/_1707_TheBedbathandbeyondDotComProblem.java
@@ -16,22 +16,24 @@ import java.util.Set;
 public class _1707_TheBedbathandbeyondDotComProblem {
 
 	public static List<String> decomposelntoDictionaryWords(String domain, Set<String> dictionary) {
+		
 		int[] lastLength = new int[domain.length()];
 
 		Arrays.fill(lastLength, -1);
-// When the algorithm finishes, lastLength[i] != -1 indicates
-// domain.substring(SI, i + 1) has a valid decomposition, and the length of
-// the last string in the decomposition will be lastLength[i].
+		// When the algorithm finishes, lastLength[i] != -1 indicates
+		// domain.substring(SI, i + 1) has a valid decomposition, and the length of
+		// the last string in the decomposition will be lastLength[i].
+		
 		for (int i = 0; i < domain.length(); ++i) {
-// If domain.substring(SI, i + 1) is a valid word, set lastLength[i] to the
-// length of that word.
+		// If domain.substring(SI, i + 1) is a valid word, set lastLength[i] to the
+		// length of that word.
 			if (dictionary.contains(domain.substring(0, i + 1))) {
 				lastLength[i] = i + 1;
 			}
-// If lastLength[i] = -1 look for j < i such that domain.substring(SI, j +
-// 1) has a valid decomposition and domain.substring(j + 1, i + 1) is a
-// dictionary word. If so, record the length of that word in
-// lastLength[i].
+			// If lastLength[i] = -1 look for j < i such that domain.substring(SI, j +
+			// 1) has a valid decomposition and domain.substring(j + 1, i + 1) is a
+			// dictionary word. If so, record the length of that word in
+			// lastLength[i].
 			if (lastLength[i] == -1) {
 				for (int j = 0; j < i; ++j) {
 					if (lastLength[j] != -1 && dictionary.contains(domain.substring(j + 1, i + 1))) {
@@ -41,9 +43,11 @@ public class _1707_TheBedbathandbeyondDotComProblem {
 				}
 			}
 		}
+		
+		
 		List<String> decompositions = new ArrayList<>();
 		if (lastLength[lastLength.length - 1] != -1) {
-// domain can be assembled by valid words.
+			// domain can be assembled by valid words.
 			int idx = domain.length() - 1;
 			while (idx >= 0) {
 				decompositions.add(domain.substring(idx + 1 - lastLength[idx], idx + 1));
@@ -51,6 +55,7 @@ public class _1707_TheBedbathandbeyondDotComProblem {
 			}
 			Collections.reverse(decompositions);
 		}
+		
 		return decompositions;
 
 	}
